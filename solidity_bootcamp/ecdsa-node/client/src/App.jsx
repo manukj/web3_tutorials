4a973074ce7827564a854260f8eb4df6d9b99734
Manu <manu1998kj@gmail.com>
2023-12-22 17:01:42 +0000

    extracted address from private key

--- solidity_bootcamp/ecdsa-node/client/src/App.jsx
+++ solidity_bootcamp/ecdsa-node/client/src/App.jsx
@@ -1,11 +1,12 @@
-import Wallet from "./Wallet";
-import Transfer from "./Transfer";
-import "./App.scss";
 import { useState } from "react";
+import "./App.scss";
+import Transfer from "./Transfer";
+import Wallet from "./Wallet";
 
 function App() {
   const [balance, setBalance] = useState(0);
   const [address, setAddress] = useState("");
+  const [privateKey, setPrivateKey] = useState("");
 
   return (
     <div className="app">
@@ -14,8 +15,14 @@ function App() {
         setBalance={setBalance}
         address={address}
         setAddress={setAddress}
+        privateKey={privateKey}
+        setPrivateKey={setPrivateKey}
       />
-      <Transfer setBalance={setBalance} address={address} />
+      <Transfer
+        setBalance={setBalance}
+        address={address}
+        privateKey={privateKey}
+      />
     </div>
   );
 }
--- solidity_bootcamp/ecdsa-node/client/src/Wallet.jsx
+++ solidity_bootcamp/ecdsa-node/client/src/Wallet.jsx
@@ -1,8 +1,19 @@
+import { secp256k1 } from "ethereum-cryptography/secp256k1";
+import { toHex } from "ethereum-cryptography/utils";
 import server from "./server";
 
-function Wallet({ address, setAddress, balance, setBalance }) {
+function Wallet({
+  address,
+  setAddress,
+  balance,
+  setBalance,
+  privateKey,
+  setPrivateKey,
+}) {
   async function onChange(evt) {
-    const address = evt.target.value;
+    const privateKey = evt.target.value;
+    setPrivateKey(privateKey);
+    address = toHex(secp256k1.getPublicKey(privateKey));
     setAddress(address);
     if (address) {
       const {
@@ -20,9 +31,14 @@ function Wallet({ address, setAddress, balance, setBalance }) {
 
       <label>
         Wallet Address
-        <input placeholder="Type an address, for example: 0x1" value={address} onChange={onChange}></input>
+        <input
+          placeholder="Enter Your Private Key"
+          value={privateKey}
+          onChange={onChange}
+        ></input>
       </label>
 
+      <div className="address">Address: {address.slice(0, 10)}...</div>
       <div className="balance">Balance: {balance}</div>
     </div>
   );
