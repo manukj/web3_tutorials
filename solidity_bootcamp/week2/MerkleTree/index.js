abce74bd047248abf9b8427c81cfb8b67077cf6d
Manu <manu1998kj@gmail.com>
2023-12-28 01:49:56 +0000

    merkle tree

--- solidity_bootcamp/week2/MerkleTree/index.js
+++ solidity_bootcamp/week2/MerkleTree/index.js
@@ -1,46 +1,46 @@
 class MerkleTree {
-  constructor(leaves, concat) {
-    this.leaves = leaves;
-    this.concat = concat;
-  }
-  getRoot(leaves = this.leaves) {
-    if (leaves.length === 1) {
-      return leaves[0];
+    constructor(leaves, concat) {
+        this.leaves = leaves;
+        this.concat = concat;
     }
-    const layer = [];
-    let i;
-    for (i = 0; i < leaves.length - 1; i += 2) {
-      const left = leaves[i];
-      const right = leaves[i + 1];
-      layer.push(this.concat(left, right));
-    }
-    if (i == leaves.length - 1) {
-      layer.push(leaves[i]);
-    }
-    return this.getRoot(layer);
-  }
-
-  getProof(index, layer = this.leaves, proof = []) {
-    if (layer.length === 1) return proof;
-    var newLayer = [];
-    for (let i = 0; i < layer.length; i += 2) {
-      const left = layer[i];
-      const right = layer[i + 1];
-      if (!right) {
-        newLayer.push(left);
-      } else {
-        newLayer.push(this.concat(left, right));
-        if (i == index || i === index - 1) {
-          let isLeft = index % 2 == 0;
-          proof.push({
-            data: isLeft ? right : left,
-            left: !isLeft,
-          });
+    getRoot(leaves = this.leaves) {
+        if (leaves.length === 1) {
+            return leaves[0];
         }
-      }
+        const layer = [];
+        let i;
+        for (i = 0; i < leaves.length-1; i += 2) {
+            const left = leaves[i];
+            const right = leaves[i + 1];
+            layer.push(this.concat(left, right));
+        }
+        if (i == leaves.length-1){
+            layer.push(leaves[i])
+        }
+        return this.getRoot(layer);
     }
-    return this.getProof(Math.floor(index / 2), newLayer, proof);
-  }
+
+    getProof(index,layer = this.leaves, proof=[]){
+        if(layer.length === 1) return proof;
+        var newLayer = [];
+        for(let i=0; i< layer.length ;i+=2){
+            const left = layer[i]
+            const right = layer[i+1]
+            if(!right){
+                newLayer.push(left)
+            }else{
+                newLayer.push(this.concat(left,right))
+                if(i == index || i=== index-1){
+                    let isLeft = (index % 2 == 0 );
+                    proof.push({
+                        data: isLeft ? right : left,
+                        left: !isLeft
+                    });
+                }
+            }
+        }
+        return this.getProof(Math.floor(index / 2), newLayer, proof);
+    }
 }
 
 module.exports = MerkleTree;
