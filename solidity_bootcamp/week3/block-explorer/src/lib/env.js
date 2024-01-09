6bd2e07a92b010e87dd97a61bd5db62914ebd894
Manu <manu1998kj@gmail.com>
2024-01-09 02:26:00 +0000

    Add Alchemy SDK and environment configuration

---
+++ solidity_bootcamp/week3/block-explorer/src/lib/env.js
@@ -0,0 +1,4 @@
+
+export const env = {
+    ALCHEMY_API_KEY : import.meta.env.VITE_ALCHEMY_API_KEY,
+}
--- solidity_bootcamp/week3/block-explorer/src/routes/+page.svelte
+++ solidity_bootcamp/week3/block-explorer/src/routes/+page.svelte
@@ -1,1 +1,22 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<script lang="ts">
+  import { env } from "$lib/env";
+  import { Alchemy, Network } from "alchemy-sdk";
+  import { onMount } from "svelte";
+
+  let blockNumber = 0;
+  const settings = {
+    apiKey: env.ALCHEMY_API_KEY,
+    network: Network.ETH_MAINNET,
+  };
+
+  onMount(() => {
+    const alchemy = new Alchemy(settings);
+    alchemy.core.getBlockNumber().then((number) => {
+      blockNumber = number;
+    });
+  });
+</script>
+
+<h1 class="text-3xl font-bold underline">
+  Current Block Number: {blockNumber}
+</h1>
