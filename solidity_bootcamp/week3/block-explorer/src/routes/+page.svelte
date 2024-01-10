dfaddaaaff62b5a4c0c4708a578d4afafc6d2714
Manu <manu1998kj@gmail.com>
2024-01-10 02:53:13 +0000

    Add BlockContent component to display block details

--- solidity_bootcamp/week3/block-explorer/src/routes/+page.svelte
+++ solidity_bootcamp/week3/block-explorer/src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { env } from "$lib/env";
   import { Alchemy, Network, type BlockWithTransactions } from "alchemy-sdk";
+  import BlockContent from "./component/BlockContent.svelte";
 
   let isLoading = false;
   let transcationHash = "";
@@ -57,21 +58,7 @@
     {#if isLoading}
       <span class="loading loading-dots loading-lg"></span>
     {:else if blockDetails}
-      <div class="card w-96 bg-base-100 shadow-xl">
-        <figure>
-          <img
-            src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-            alt="Shoes"
-          />
-        </figure>
-        <div class="card-body">
-          <h2 class="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div class="card-actions justify-end">
-            <button class="btn btn-primary">Buy Now</button>
-          </div>
-        </div>
-      </div>
+      <BlockContent {blockDetails} />
     {:else if error}
       <div role="alert" class="alert alert-error">
         <svg
---
+++ solidity_bootcamp/week3/block-explorer/src/routes/component/BlockContent.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import { type BlockWithTransactions } from "alchemy-sdk";
+
+  // expect a prop called blockDetails
+  export let blockDetails: BlockWithTransactions;
+
+  var gasUsedPercentage: number =
+    (blockDetails.gasUsed.toNumber() / blockDetails.gasLimit.toNumber()) * 100;
+</script>
+
+<div class="card p-5 w-96 bg-base-100 shadow-xl">
+  <figure>
+    <div class="pr-2">GAS USED</div>
+    <div
+      class="radial-progress"
+      style="--value:{gasUsedPercentage};"
+      role="progressbar"
+    >
+      {gasUsedPercentage.toFixed(2)}%
+    </div>
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Block : {blockDetails.number}</h2>
+
+    <div class="flex flex-row">
+      Total Transactions in this block
+      <div class=" font-bold pl-3">{blockDetails.transactions.length}</div>
+    </div>
+    <div class="font-bold">
+      TimeStamp : {new Date(blockDetails.timestamp * 1000).toLocaleString()}
+    </div>
+  </div>
+
+  <div class="card-actions justify-center">
+    <a href="https://etherscan.io/tx/{blockDetails.hash}" class="btn btn-link"
+      >View More Details</a
+    >
+  </div>
+</div>
