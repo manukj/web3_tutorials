2aef8f01d22dce6e47e67de3f38992aef0b3749f
Manu <manu1998kj@gmail.com>
2024-01-10 01:48:36 +0000

    Add daisyui and css-selector-tokenizer dependencies

--- solidity_bootcamp/week3/block-explorer/src/routes/+page.svelte
+++ solidity_bootcamp/week3/block-explorer/src/routes/+page.svelte
@@ -4,6 +4,7 @@
   import { onMount } from "svelte";
 
   let blockNumber = 0;
+  let transcationHash = "";
   const settings = {
     apiKey: env.ALCHEMY_API_KEY,
     network: Network.ETH_MAINNET,
@@ -15,8 +16,27 @@
       blockNumber = number;
     });
   });
+
+  function getBlockDetails() {
+    const alchemy = new Alchemy(settings);
+    alchemy.core.getBlockWithTransactions(transcationHash).then((tx) => {
+      console.log(tx);
+      blockNumber = tx.number;
+    });
+  }
 </script>
 
-<h1 class="text-3xl font-bold underline">
+<h1 class="">
+  <div class="flex flex-row">
+    <input
+      type="text"
+      placeholder="Enter Transaction Hash"
+      bind:value={transcationHash}
+    />
+    <button class=" butt" on:click={getBlockDetails}>
+      Get Block Details
+    </button>
+  </div>
+
   Current Block Number: {blockNumber}
 </h1>
